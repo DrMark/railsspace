713773be63e7aa4609e4b64547e7c39e20de31fc
DrMark <drmark@gmail.com>
2008-10-24 15:57:15 -1000

    adding a basic user_controller test (5.4.2)

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  # Make sure the registration page responds with the proper form.
+  def test_registration_page
+    get :register
+    title = assigns(:title)
+    assert_equal "Register", title
+    assert_response :success
+    assert_template "register"
   end
 end
