02c39ea83e1d769e76dd0670b88df1bbbd6a2dc8
DrMark <drmark@gmail.com>
2008-10-24 14:26:27 -1000

    adding validations (3.2.2 L3.5)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :screen_name, :email
+  validates_length_of     :screen_name, :within => 4..20
+  validates_length_of     :password,    :within => 4..40
+  validates_length_of     :email,       :maximum => 50
+  validates_presence_of   :email
 end
