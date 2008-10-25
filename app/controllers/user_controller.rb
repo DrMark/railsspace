14c9c959b40c8032daf60d904c2dae73d3863bdf
DrMark <drmark@gmail.com>
2008-10-24 16:36:23 -1000

    adding the login view & action (6.2.4)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,4 +15,22 @@ class UserController < ApplicationController
       end
     end
   end
+
+  def login
+    @title = "Log in to RailsSpace"
+    if request.post? and params[:user]
+      @user = User.new(params[:user])
+      user = User.find_by_screen_name_and_password(@user.screen_name,
+                                                   @user.password)
+      if user
+        session[:user_id] = user.id
+        flash[:notice] = "User #{user.screen_name} logged in!"
+        redirect_to :action => "index"
+      else
+        # Don't show the password in the view.
+        @user.password = nil
+        flash[:notice] = "Invalid screen name/password combination"
+      end
+    end
+  end
 end
