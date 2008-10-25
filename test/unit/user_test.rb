1a803f4fbfbac12afb1e1d562b7bbc66ccdc3963
DrMark <drmark@gmail.com>
2008-10-24 16:15:54 -1000

    testing the screen name length (5.6.2)

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -31,4 +31,39 @@ class UserTest < ActiveSupport::TestCase
     assert_equal @error_messages[:taken], user_repeat.errors.on(:screen_name)
     assert_equal @error_messages[:taken], user_repeat.errors.on(:email)
   end
+
+  # Make sure the screen name can't be too short.
+  def test_screen_name_minimum_length
+    user = @valid_user
+    min_length = User::SCREEN_NAME_MIN_LENGTH
+
+    # Screen name is too short.
+    user.screen_name = "a" * (min_length - 1)
+    assert !user.valid?, "#{user.screen_name} should raise a minimum length error"
+    # Format the error message based on minimum length.
+    correct_error_message = sprintf(@error_messages[:too_short], min_length)
+    assert_equal correct_error_message, user.errors.on(:screen_name)
+
+    # Screen name is minimum length.
+    user.screen_name = "a" * min_length
+    assert user.valid?, "#{user.screen_name} should be just long enough to pass"
+  end
+
+  # Make sure the screen name can't be too long.
+  def test_screen_name_maximum_length
+    user = @valid_user
+    max_length = User::SCREEN_NAME_MAX_LENGTH
+
+    # Screen name is too long.
+    user.screen_name = "a" * (max_length + 1)
+    assert !user.valid?, "#{user.screen_name} should raise a maximum length error"
+
+    # Format the error message based on maximum length
+    correct_error_message = sprintf(@error_messages[:too_long], max_length)
+    assert_equal correct_error_message, user.errors.on(:screen_name)
+
+    # Screen name is maximum length.
+    user.screen_name = "a" * max_length
+    assert user.valid?, "#{user.screen_name} should be just short enough to pass"
+  end
 end
