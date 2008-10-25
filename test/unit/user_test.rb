3763c18eb2729f8b478fc9acffa17b3900ef3e2f
DrMark <drmark@gmail.com>
2008-10-24 16:03:12 -1000

    adding basic user unit tests (5.5.1)

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def setup
+    @valid_user = users(:valid_user)
+    @invalid_user = users(:invalid_user)
+  end
+
+  # This user should be valid by construction.
+  def test_user_validity
+    assert users(:valid_user).valid?
+  end
+
+  # This user should be invalid by construction.
+  def test_user_invalidity
+    assert !@invalid_user.valid?
+    attributes = [:screen_name, :email, :password]
+    attributes.each do |attribute|
+      assert @invalid_user.errors.invalid?(attribute)
+    end
   end
 end
