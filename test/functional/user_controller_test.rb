d0e4819f5cf684b4b17c2359ae473de547e50b54
DrMark <drmark@gmail.com>
2008-10-24 15:58:08 -1000

    adding a more detailed user_controller test (5.4.2 L5.6)

--- test/functional/user_controller_test.rb
+++ test/functional/user_controller_test.rb
@@ -9,5 +9,26 @@ class UserControllerTest < ActionController::TestCase
     assert_equal "Register", title
     assert_response :success
     assert_template "register"
+
+    # Test the form and all its tags.
+    assert_tag "form", :attributes => { :action => "/user/register",
+                                        :method => "post" }
+    assert_tag   "input",
+                :attributes => { :name => "user[screen_name]",
+                                 :type => "text",
+                                 :size => User::SCREEN_NAME_SIZE,
+                                 :maxlength => User::SCREEN_NAME_MAX_LENGTH }
+    assert_tag   "input",
+                :attributes => { :name => "user[email]",
+                                 :type => "text",
+                                 :size => User::EMAIL_SIZE,
+                                 :maxlength => User::EMAIL_MAX_LENGTH }
+    assert_tag   "input",
+                :attributes => { :name => "user[password]",
+                                 :type => "password",
+                                 :size => User::PASSWORD_SIZE,
+                                 :maxlength => User::PASSWORD_MAX_LENGTH }
+    assert_tag   "input", :attributes => { :type => "submit",
+                                          :value => "Register!" }
   end
 end
