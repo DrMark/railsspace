3f98c3207bfefaf0dad82d963485bb55c577762e
DrMark <drmark@gmail.com>
2008-10-24 14:25:03 -1000

    updating the user migration (3.1.3)

--- db/migrate/20081025002222_create_users.rb
+++ db/migrate/20081025002222_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.string :screen_name
+      t.string :email
+      t.string :password
       t.timestamps
     end
   end
