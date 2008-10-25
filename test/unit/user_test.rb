ca50c268ba0dfeab3688f0c5b883de4f749ef736
DrMark <drmark@gmail.com>
2008-10-24 16:06:07 -1000

    testing the uniqueness of screen name and email (5.6.1)

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -20,4 +20,14 @@ class UserTest < ActiveSupport::TestCase
       assert @invalid_user.errors.invalid?(attribute)
     end
   end
+
+  # One test that checks the uniqueness of both screen name and email
+  def test_uniqueness_of_screen_name_and_email
+    user_repeat = User.new(:screen_name => @valid_user.screen_name,
+                           :email => @valid_user.email,
+                           :password => @valid_user.password)
+    assert !user_repeat.valid?
+    assert_equal "has already been taken", user_repeat.errors.on(:screen_name)
+    assert_equal "has already been taken", user_repeat.errors.on(:email)
+  end
 end
