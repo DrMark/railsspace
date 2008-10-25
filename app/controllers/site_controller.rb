aa2ad24bb045fa1850e496f49db951dd43346175
DrMark <drmark@gmail.com>
2008-10-24 14:12:40 -1000

    updating the site controller to add titles

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,13 @@
 class SiteController < ApplicationController
   def index
+    @title = "RailsSpace"
   end
 
   def about
+    @title = "About RailsSpace"
   end
 
   def help
+    @title = "RailsSpace Help"
   end
-
 end
