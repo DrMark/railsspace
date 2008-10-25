5a2830b6042ab6376095dcc8dd4d29e503e1787b
DrMark <drmark@gmail.com>
2008-10-24 14:27:00 -1000

    adding constants to the validations (3.2.2 L3.6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
+  # Max & min lengths for all fields
+  SCREEN_NAME_MIN_LENGTH = 4
+  SCREEN_NAME_MAX_LENGTH = 20
+  PASSWORD_MIN_LENGTH = 4
+  PASSWORD_MAX_LENGTH = 40
+  EMAIL_MAX_LENGTH = 50
+  SCREEN_NAME_RANGE = SCREEN_NAME_MIN_LENGTH..SCREEN_NAME_MAX_LENGTH
+  PASSWORD_RANGE = PASSWORD_MIN_LENGTH..PASSWORD_MAX_LENGTH
+
   validates_uniqueness_of :screen_name, :email
-  validates_length_of     :screen_name, :within => 4..20
-  validates_length_of     :password,    :within => 4..40
-  validates_length_of     :email,       :maximum => 50
-  validates_presence_of   :email
+  validates_length_of :screen_name, :within => SCREEN_NAME_RANGE
+  validates_length_of :password, :within => PASSWORD_RANGE
+  validates_length_of :email, :maximum => EMAIL_MAX_LENGTH
+  validates_presence_of :email
 end
