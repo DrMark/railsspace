340871e5482f2b88fb7db1beb38d5ee791a83fe0
DrMark <drmark@gmail.com>
2008-10-24 15:54:51 -1000

    adding site_controller tests, with 1 failing test (5.3 L5.3)

--- test/functional/site_controller_test.rb
+++ test/functional/site_controller_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class SiteControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_index
+    get :index
+    title = assigns(:title)
+    assert_equal "Welcome to RailsSpace!", title
+    assert_response :success
+    assert_template "index"
+    end
+
+  def test_about
+    get :about
+    title = assigns(:title)
+    assert_equal "About RailsSpace", title
+    assert_response :success
+    assert_template "about"
+  end
+
+  def test_help
+    get :help
+    title = assigns(:title)
+    assert_equal "RailsSpace Help", title
+    assert_response :success
+    assert_template "help"
   end
 end
