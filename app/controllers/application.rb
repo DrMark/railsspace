2c43550d73369a2c5990317c8b6adaba012f4f6e
DrMark <drmark@gmail.com>
2008-11-14 16:29:07 -1000

    7.2.2 Remembering that we remembered

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,15 +1,18 @@
-# Filters added to this controller apply to all controllers in the application.
+# Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  include ApplicationHelper
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'dba2270b908e5a6c55cf3a77e3517915'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  before_filter :check_authorization
+
+  # Pick a unique cookie name to distinguish our session data from others'
+  session :session_key => '_rails_space_session_id'
+
+  def check_authorization
+    authorization_token = cookies[:authorization_token]
+    if authorization_token and not logged_in?
+      user = User.find_by_authorization_token(authorization_token)
+      user.login!(session) if user
+    end
+  end
 end
