3ae313d34f12399e666474d5a286302e62c04115
DrMark <drmark@gmail.com>
2008-11-14 16:46:42 -1000

    7.5.3 Just two more bits of polish

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,18 +24,14 @@ class UserController < ApplicationController
   def login
     @title = "Log in to RailsSpace"
     if request.get?
-      @user = User.new(:remember_me => cookies[:remember_me] || "0")
+      @user = User.new(:remember_me => remember_me_string)
     elsif param_posted?(:user)
       @user = User.new(params[:user])
       user = User.find_by_screen_name_and_password(@user.screen_name,
                                                    @user.password)
       if user
         user.login!(session)
-        if @user.remember_me?
-          user.remember!(cookies)
-        else
-          user.forget!(cookies)
-        end
+        @user.remember_me? ? user.remember!(cookies) : user.forget!(cookies)
         flash[:notice] = "User #{user.screen_name} logged in!"
         redirect_to_forwarding_url
       else
@@ -76,5 +72,10 @@ class UserController < ApplicationController
     else
       redirect_to :action => "index"
     end
+  end
+
+  # Return a string with the status of the remember me checkbox.
+  def remember_me_string
+    cookies[:remember_me] || "0"
   end
 end
